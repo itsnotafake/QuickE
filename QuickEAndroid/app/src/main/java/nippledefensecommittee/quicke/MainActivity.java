64d09397a46b492040b12f64fb69c3e37d56ce69
Devin <itsnotafake@gmail.com>
2017-07-20 14:21:06 -0700

    feat: Price Range class in MainActivity
    added a class that holds a boolean arraylist that indicates whether or not a specific price range has been selected by the user.

--- QuickEAndroid/app/src/main/java/nippledefensecommittee/quicke/MainActivity.java
+++ QuickEAndroid/app/src/main/java/nippledefensecommittee/quicke/MainActivity.java
@@ -2,8 +2,12 @@ package nippledefensecommittee.quicke;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //is this how you add a comment
+    //static class that represents which price options have been selected
+    public static class PriceRange{
+
+        static ArrayList<Boolean> priceRange;
+
+        static{
+            priceRange = new ArrayList<>();
+            for(int i = 0; i < 4; i++){
+                priceRange.set(i, false);
+            }
+        }
+
+        public static void updatePriceIndication(int position, boolean isGoodPrice){
+            try{
+                priceRange.set(position, isGoodPrice);
+            }catch(IndexOutOfBoundsException e){
+                Log.e(TAG, "error: " + e);
+            }
+        }
+
+        /**
+         * Any changes made to priceRange should be done with 'updatePriceIndication'
+         * @return a copy of Price Range
+         */
+        public boolean getPriceIndicationAt(int position){
+            return priceRange.get(position);
+        }
+    }
 }
