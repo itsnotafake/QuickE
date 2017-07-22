f78b16541fd1dbb303dc9d4d034b172f2da2ad3a
Devin <itsnotafake@gmail.com>
2017-07-22 15:43:07 -0700

    feat: Refresh Icon added to Main Toolbar

--- QuickEAndroid/app/src/main/java/nippledefensecommittee/quicke/MainActivity.java
+++ QuickEAndroid/app/src/main/java/nippledefensecommittee/quicke/MainActivity.java
@@ -10,6 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -32,6 +35,25 @@ public class MainActivity extends AppCompatActivity {
 
         initializeAppBar();
         getUserPermission();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_refresh:
+                //TODO
+                return true;
+            default:
+                Log.e(TAG, "Unrecognized item onOptionsItemSelected");
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void initializeAppBar(){
