072997094f7b1a820198465392c90a39bbcd042e
Devin <itsnotafake@gmail.com>
2017-07-22 15:33:51 -0700

    feat: Toolbar
    made changes to toolbar implementation and text color

--- QuickEAndroid/app/src/main/java/nippledefensecommittee/quicke/MainActivity.java
+++ QuickEAndroid/app/src/main/java/nippledefensecommittee/quicke/MainActivity.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -27,7 +30,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeAppBar();
         getUserPermission();
+    }
+
+    private void initializeAppBar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.colorAccent));
+        setSupportActionBar(toolbar);
     }
 
     /**
