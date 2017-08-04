3c836e2dcb6661dddb5cf56974a84fedd5d06519
Eric Duong <Eric Duong>
2017-08-03 17:09:01 -0700

    Changed fragment activity to fragment

---
+++ QuickEAndroid/app/src/main/java/nippledefensecommittee/quicke/framework/PictureSlideShowFragment.java
@@ -0,0 +1,105 @@
+package nippledefensecommittee.quicke.framework;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.NavUtils;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import nippledefensecommittee.quicke.R;
+
+/**
+ * Created by ETD on 8/3/2017.
+ */
+
+public class PictureSlideShowFragment extends Fragment {
+
+    private static final String TAG = PictureSlideShowFragment.class.getName();
+
+    private static final int NUM_SLIDES = 5;
+    private ViewPager mViewPager;
+    private PagerAdapter mPagerAdapter;
+    LinearLayout dotsPanel;
+    private int dotsCount;
+    private ImageView[] dots;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_picture_slide_show, container, false);
+
+        initializeViewPager(view);
+        initializeDotsPanel(view);
+
+        return view;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpTo(getActivity(), new Intent(getActivity(), MainActivity.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeViewPager(View view) {
+        mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        mPagerAdapter = new PictureSlidePagerAdapter(getActivity().getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+
+    private void initializeDotsPanel(View view) {
+        dotsPanel = (LinearLayout) view.findViewById(R.id.dotsIndicator);
+        dotsCount = mPagerAdapter.getCount();
+        dots = new ImageView[dotsCount];
+
+        for (int i = 0; i < dotsCount; i++) {
+            dots[i] = new ImageView(getActivity());
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            lParams.setMargins(8, 0, 8, 0);
+
+            dotsPanel.addView(dots[i], lParams);
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < dotsCount; i++) {
+                    if (i == position) {
+                        dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+                    } else {
+                        dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.nonactive_dot));
+                    }
+                }
+            }
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+}
--- QuickEAndroid/app/src/main/java/nippledefensecommittee/quicke/framework/RecommendationActivity.java
+++ QuickEAndroid/app/src/main/java/nippledefensecommittee/quicke/framework/RecommendationActivity.java
@@ -1,5 +1,7 @@
 package nippledefensecommittee.quicke.framework;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,10 +10,23 @@ import nippledefensecommittee.quicke.R;
 
 public class RecommendationActivity extends AppCompatActivity {
 
-    // TODO: Add fragment activities for picture slides and Yes/No buttons
+    private static final String TAG = RecommendationActivity.class.getName();
+    private int mContainerId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendation);
+        mContainerId = R.id.fragment_container;
+
+        initializeSlideShowFragment();
+    }
+
+    private void initializeSlideShowFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        PictureSlideShowFragment fragment = new PictureSlideShowFragment();
+        fragmentTransaction.add(mContainerId, fragment);
+        fragmentTransaction.commit();
     }
 }
